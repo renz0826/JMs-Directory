c24cda64d34cafba68d8e93584a5f150806590ca
renz0826 <brenzbedonia@gmail.com>
2025-11-10 22:06:58 +0800

    feat: add Pharmacy class with method templates

---
+++ Classes/src/Pharmacy.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Pharmacy extends Account {
+    private ArrayList<Medicine> medicines = new ArrayList<>();
+
+    // Methods
+    public void addMedicine() {}
+    public void searchMedicine() {}
+    public void updateMedicineAmount() {}
+    public void updateMedicinePrice() {}
+    public void deleteMedicine() {}
+}
