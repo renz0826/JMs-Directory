eacce583e1b1eb8cd4746c71b996b541edbb89bc
renz0826 <brenzbedonia@gmail.com>
2025-11-10 22:03:19 +0800

    feat: add Main class for testing

---
+++ Classes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {        
+        // Customer c = new Customer("a", "b");
+        // c.login();
+
+        //UIManager.displayLoginChoice();
+        //UIManager.displayAdminMenu();
+    }
+}
