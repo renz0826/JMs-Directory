c6a00f0bdc307390075242d4f60ffa4c4ad5f4a0
renz0826 <brenzbedonia@gmail.com>
2025-11-10 22:01:55 +0800

    feat: update Account class through implementing login logic and credential validation

--- Classes/src/Account.java
+++ Classes/src/Account.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 // Base class account
-class Account{
+public class Account{
     // Sets username and password's max size to 128 characters
     final protected int MAX_USERNAME_SIZE = 128;
     final protected int MAX_PASSWORD_SIZE = 128;
@@ -9,24 +11,38 @@ class Account{
     protected String password;
 
     // Base class constructor
-    private Account(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
+    protected Account(){}
 
     // Login method
-    public void login(){
+    public void login() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("====== Login System ======");
 
-    }
+        boolean isValid;
+        do {
+            // Ask for username
+            System.out.print("Enter username: ");
+            String username = sc.nextLine();
+            
+            // Ask for password
+            System.out.print("Enter password: ");
+            String password = sc.nextLine();
+
+            isValid = isCredentialsCorrect(username, password); 
+            if (!isValid) {
+                System.out.println("Credentials are not correct, Try again!");
+            }
+        } while (!isValid);
+
+        sc.close();
+    };
 
     // Logout method
-    public void logout(){
-
-    }
+    public void logout() {};
 
     // Credential validation method
-    public boolean isCredentialsCorrect(){
-        return false;
-    }
-
+    public boolean isCredentialsCorrect(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    };
 }
