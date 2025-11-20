22f011d6fa596e7a532c0f1385ba016b44418370
renz0826 <brenzbedonia@gmail.com>
2025-11-21 00:10:07 +0800

    fix(UI): use Ascii Table library for a better design

--- src/main/java/com/example/classes/Customer.java
+++ src/main/java/com/example/classes/Customer.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
+
 public class Customer extends Account {
     private List<Medicine> medicines;
     private double funds;
@@ -20,5 +23,25 @@ public class Customer extends Account {
         super(name, username, password);
         this.medicines = medicines;
         this.funds = funds;
+    }
+
+    public static void read(){
+        AsciiTable at = new AsciiTable();
+        
+
+        at.addRule();
+        at.addRow(null, "Account Details");
+        at.setTextAlignment(TextAlignment.CENTER);
+        at.addRule();
+        at.addRow("row 2 col 1", "row 2 col 2");
+        at.addRule();
+
+        String rend = at.render();
+
+
+
+        System.out.println(rend);
+
+        
     }
 }
