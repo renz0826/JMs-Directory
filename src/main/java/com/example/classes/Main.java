3170027511c47c224d07504eb63b115d3aa2cab3
Dyestrm <darkyissus29@gmail.com>
2025-11-22 23:45:03 +0800

    test: test program flow in main

--- src/main/java/com/example/classes/Main.java
+++ src/main/java/com/example/classes/Main.java
@@ -6,19 +6,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {        
-        // Customer c = new Customer("a", "b");
-        // c.login();
-
-        //UIManager.displayLoginChoice();
-        //UIManager.displayAdminMenu();
-        Pharmacy pharmacy;
-        Path path = Path.of("accounts/pharmacies/HealthPlusPharmacy.json");
-        try {
-            pharmacy = Database.getObjectMapper().readValue(path.toFile(), Pharmacy.class);
-            List<Medicine> medicines = pharmacy.getMedicines();
-            UIManager.displayData(medicines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        UIManager.displayLoginChoice();
     }
 }
