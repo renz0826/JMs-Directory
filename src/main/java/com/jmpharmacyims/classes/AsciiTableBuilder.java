3a0f2de4a8b3efa95518359f9dec185db3bd5b7e
renz0826 <brenzbedonia@gmail.com>
2025-11-27 21:29:08 +0800

    refactor(tableBuilder): let serRows accept ArrayList Strings
    - remove setRow

--- src/main/java/com/jmpharmacyims/classes/AsciiTableBuilder.java
+++ src/main/java/com/jmpharmacyims/classes/AsciiTableBuilder.java
@@ -1,13 +1,14 @@
 package com.jmpharmacyims.classes;
 
-import de.vandermeer.asciitable.AsciiTable;
-import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
+
 public class AsciiTableBuilder {
+
     private String header;
     private List<String> bodyRows = new ArrayList<>();
     private String footer;
@@ -17,11 +18,6 @@ public class AsciiTableBuilder {
 
     public AsciiTableBuilder setHeader(String content) {
         this.header = content;
-        return this;
-    }
-
-    public AsciiTableBuilder setRow(String content) {
-        this.bodyRows.add(content);
         return this;
     }
 
@@ -46,28 +42,28 @@ public class AsciiTableBuilder {
         if (footer == null) {
             throw new IllegalStateException("Menu table is missing a footer!");
         }
-  
+
         AsciiTable at = new AsciiTable();
 
         // Add header
         at.addRule();
         at.addRow(header)
-          .setTextAlignment(TextAlignment.CENTER);
+                .setTextAlignment(TextAlignment.CENTER);
         at.addRule();
 
         // Add body rows
         for (String content : bodyRows) {
             at.addRow(content)
-              .setTextAlignment(TextAlignment.LEFT)
-              .setPadding(1)  // General padding
-              .setPaddingLeft(7);  // Override left padding
+                    .setTextAlignment(TextAlignment.LEFT)
+                    .setPadding(1) // General padding
+                    .setPaddingLeft(7);  // Override left padding
         }
         at.addRule();
 
         // Add footer
         at.addRow(footer)
-          .setPadding(1)
-          .setPaddingLeft(7);
+                .setPadding(1)
+                .setPaddingLeft(7);
         at.addRule();
 
         // Return rendered table
@@ -82,17 +78,17 @@ public class AsciiTableBuilder {
         if (bodyRows.isEmpty()) {
             throw new IllegalStateException("Menu table is missing rows!");
         }
-        
+
         AsciiTable at = new AsciiTable();
 
         at.addRule();
         at.addRow(header).setTextAlignment(TextAlignment.CENTER);
         at.addRule();
-        
+
         at.addRow(bodyRows.getFirst())
-            .setPadding(1)
-            .setPaddingLeft(7)
-            .setTextAlignment(TextAlignment.LEFT);
+                .setPadding(1)
+                .setPaddingLeft(7)
+                .setTextAlignment(TextAlignment.LEFT);
         at.addRule();
 
         return at.render();
@@ -107,7 +103,7 @@ public class AsciiTableBuilder {
         at.setTextAlignment(TextAlignment.CENTER);
 
         return at.render();
-    } 
+    }
 
     public static String buildMedicineTable(List<Medicine> medicines) {
         AsciiTable asciiTable = new AsciiTable();
@@ -119,8 +115,8 @@ public class AsciiTableBuilder {
         int indexCounter = 0;
         for (Medicine medicine : medicines) {
             asciiTable.addRow(
-                indexCounter, medicine.getName(), medicine.getPrice(), medicine.getAmount(),
-                medicine.getBrand(), medicine.getExpirationDate(), medicine.getPurpose()
+                    indexCounter, medicine.getName(), medicine.getPrice(), medicine.getAmount(),
+                    medicine.getBrand(), medicine.getExpirationDate(), medicine.getPurpose()
             );
             asciiTable.addRule();
             indexCounter++;
@@ -139,8 +135,8 @@ public class AsciiTableBuilder {
         int indexCounter = 0;
         for (Customer customer : customers) {
             asciiTable.addRow(
-                indexCounter, customer.getName(), customer.getUsername(), 
-                customer.getPassword(), customer.getFunds()
+                    indexCounter, customer.getName(), customer.getUsername(),
+                    customer.getPassword(), customer.getFunds()
             );
             asciiTable.addRule();
             indexCounter++;
